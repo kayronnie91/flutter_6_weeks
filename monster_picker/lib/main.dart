b3ab5be4e0f83d90e832f8a63646f31a571799c5
Ronnie Lima <skylink@Ronnies-MacBook-Air.local>
2026-05-17 14:04:24 +0700

    Create Monster Picker app skeleton UI

--- monster_picker/lib/main.dart
+++ monster_picker/lib/main.dart
@@ -14,6 +14,29 @@ class MonsterApp extends StatefulWidget {
 class _MonsterAppState extends State<MonsterApp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('Monster Picker')));
+    return Scaffold(
+      backgroundColor: Color(0xFF131313),
+      appBar: AppBar(
+        backgroundColor: Color(0xFF131313),
+        leading: Icon(Icons.menu, color: Color(0xFFEBB2FF)),
+        title: const Text(
+          'Monster Picker',
+          style: TextStyle(color: Color(0xFFEBB2FF), fontFamily: 'Sora'),
+        ),
+        actions: [Icon(Icons.settings, color: Color(0xFFEBB2FF))],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Container(), // monster image container
+            Text(''), // Monster title
+            Text(''), // Monster description
+            Container(), // Progress Bar Container
+            Row(), // Row with monster cards - horizonal scroll view
+          ],
+        ),
+      ),
+    );
   }
 }
