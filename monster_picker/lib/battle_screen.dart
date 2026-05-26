df2f38b07a22e3237d9bda00fdd25112c69e48bc
Ronnie Lima <kayronnie91@gmail.com>
2026-05-26 17:23:17 +0700

    Refactored battle screen for dynamic state updates

--- monster_picker/lib/battle_screen.dart
+++ monster_picker/lib/battle_screen.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:monster_picker/monster.dart';
 import 'package:monster_picker/stat_bar.dart';
 
-class BattleScreen extends StatelessWidget {
+class BattleScreen extends StatefulWidget {
   final Monster player1Monster;
   final Monster player2Monster;
+
   const BattleScreen({
     super.key,
     required this.player1Monster,
@@ -12,46 +13,54 @@ class BattleScreen extends StatelessWidget {
   });
 
   @override
+  State<BattleScreen> createState() => _BattleScreenState();
+}
+
+class _BattleScreenState extends State<BattleScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFF131313),
-      appBar: AppBar(
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Scaffold(
         backgroundColor: Color(0xFF131313),
-        leading: Icon(Icons.menu, color: Color(0xFFEBB2FF)),
-        title: const Text(
-          'Battle Arena',
-          style: TextStyle(color: Color(0xFFEBB2FF), fontFamily: 'Sora'),
+        appBar: AppBar(
+          backgroundColor: Color(0xFF131313),
+          leading: Icon(Icons.menu, color: Color(0xFFEBB2FF)),
+          title: const Text(
+            'Battle Arena',
+            style: TextStyle(color: Color(0xFFEBB2FF), fontFamily: 'Sora'),
+          ),
+          actions: [Icon(Icons.settings, color: Color(0xFFEBB2FF))],
         ),
-        actions: [Icon(Icons.settings, color: Color(0xFFEBB2FF))],
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Image.asset(
-              player1Monster.monsterImage,
-              height: 350,
-              width: double.infinity,
-            ),
-            Container(
-              child: StatBar(
-                barLevel: player1Monster.maxHp,
-                statName: 'HP Remaining',
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset(
+                widget.player1Monster.monsterImage,
+                height: 350,
+                width: double.infinity,
               ),
-            ),
-            SizedBox(height: 20),
-            Text('VS'),
-            Image.asset(
-              player2Monster.monsterImage,
-              height: 350,
-              width: double.infinity,
-            ),
-            Container(
-              child: StatBar(
-                barLevel: player2Monster.maxHp,
-                statName: 'HP Remaining',
+              Container(
+                child: StatBar(
+                  barLevel: widget.player1Monster.maxHp,
+                  statName: 'HP Remaining',
+                ),
               ),
-            ),
-          ],
+              SizedBox(height: 20),
+              Text('VS'),
+              Image.asset(
+                widget.player2Monster.monsterImage,
+                height: 350,
+                width: double.infinity,
+              ),
+              Container(
+                child: StatBar(
+                  barLevel: widget.player2Monster.maxHp,
+                  statName: 'HP Remaining',
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
