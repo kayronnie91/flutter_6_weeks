16861a9477ec5a7d4c012a0ca9b01bb66514ff26
Ronnie Lima <kayronnie91@gmail.com>
2026-06-01 08:06:03 +0700

    Create story scene data model

---
+++ nightmare_story/lib/story_scene.dart
@@ -0,0 +1,17 @@
+class StoryScene {
+  String currentScene;
+  String text;
+  String choice1;
+  String choice2;
+  String nextScene1;
+  String nextScene2;
+
+  StoryScene({
+    required this.currentScene,
+    required this.text,
+    required this.choice1,
+    required this.choice2,
+    required this.nextScene1,
+    required this.nextScene2,
+  });
+}
